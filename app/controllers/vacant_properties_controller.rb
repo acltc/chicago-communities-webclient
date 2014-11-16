56343d77f1c1c0221523a1d69019567cee7a721b
mattcar <matt.carroll89@gmail.com>
2014-11-16 14:54:44 -0600

    got the vacant properties working

--- app/controllers/vacant_properties_controller.rb
+++ app/controllers/vacant_properties_controller.rb
@@ -2,7 +2,7 @@ class VacantPropertiesController < ApplicationController
 
 	def index
 
-		@vacant_properties = Unirest.get("http://localhost:3001/vacant_property.json").body
+		@vacant_properties = Unirest.get("http://localhost:3000/vacant_properties.json").body
 
 	end
 
